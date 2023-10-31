d12fca5cedd38da18262e81b06d248835e5b5505
ChiragSareen7 <144792631+ChiragSareen7@users.noreply.github.com>
2023-10-31 15:11:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+let input=document.getElementById('input')
+let addbtn=document.getElementById('button')
+let todolist=document.getElementById('todolist')
+
+let storage=[]
+addbtn.onclick=click
+
+function click(){
+    storage.push(input.value)
+    input.value="";
+    showTodo()
+}
+function showTodo(){
+    todolist.innerHTML=''
+    storage.forEach(function(todo,index){
+    todolist.innerHTML +=
+    "<li>"
+        +todo+
+        "<a onclick='edithandle("${index}")'>edittodo</a>"+
+        "<a onclick='deletehandle("${index}")'>deletetodo</a>"+
+    "</li>"
+    })
+    
+}
+console.log(storage)
+
+function edithandle(index){
+    let newvalue=prompt('enter new todo')
+    if(newvalue.length>0){
+    storage.splice(index,1,newvalue)
+    showTodo()}
+}
+function deletehandle(index){
+    storage.splice(index,1)
+    showTodo()
+}
+
+let name=chirag
+console.log("my name"${name})
